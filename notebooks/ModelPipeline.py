a3f70eee3f2107e7be20edccc68766bf361b4d9c
SKOREUK <prash94@gmail.com>
2019-12-19 21:23:08 +0000

    Initial upload

--- notebooks/ModelPipeline.py
+++ notebooks/ModelPipeline.py
@@ -1,9 +1,14 @@
 from HeartDiseaseProject.src.ProjectConfig.Config import LogisticRegression, accuracy_score
+import pickle
 
 
-
-def LRBaseline(dt,tgt,plty,clswt):
+def lr_model(dt, tgt, plty, clswt):
     clf = LogisticRegression(penalty=plty, class_weight=clswt)
     clf.fit(dt, tgt)
-    Model_Accuracy = accuracy_score(tgt, clf.predict(dt))
-    print(Model_Accuracy)
+    model_accuracy = accuracy_score(tgt, clf.predict(dt))
+    print(model_accuracy)
+
+    pickle.dump(clf, open('~/Documents/UCIHeartDisease/HeartDiseaseProject/src/model.pkl', 'rb'))
+    model = pickle.load(open('model.pkl','rb'))
+    print (model.predict([[22584,2,178,95,130,90,3,3,0,0,1]]))
+
