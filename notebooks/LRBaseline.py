5b736ac591962b46a0ff8a46812678a55234010f
SKOREUK <prash94@gmail.com>
2019-12-21 20:31:38 +0000

    Initial upload

--- notebooks/LRBaseline.py
+++ notebooks/LRBaseline.py
@@ -1,11 +1,10 @@
 import sys
 sys.path.append('../src')
 from ProjectConfig.Config import HDData
-
-from HeartDiseaseProject.notebooks.ModelPipeline import LRBaseline
+from HeartDiseaseProject.notebooks.ModelPipeline import lr_model
 HDData1 = HDData[['age','gender','height','weight','ap_hi','ap_lo','cholesterol','gluc','smoke','alco','active']]
 target = HDData['cardio']
 
 
 
-LRBaseline(HDData1,target,plty='l2', clswt='balanced')
+lr_model(HDData1, target, plty='l2', clswt='balanced')
