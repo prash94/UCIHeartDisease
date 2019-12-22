0e61ab2bb84c180690513a1d639d6991c027cd30
SKOREUK <prash94@gmail.com>
2019-12-22 22:03:32 +0000

    Inital load

--- src/ProjectConfig/Config.py
+++ src/ProjectConfig/Config.py
@@ -2,9 +2,10 @@ from pathlib import Path
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import Imputer
-from sklearn.linear_model import LogisticRegression
+import sklearn.linear_model as lm
 from sklearn.metrics import accuracy_score
 
 data_dir_raw = Path('../data')
 
 HDData = pd.read_csv('~/Documents/UCIHeartDisease/HeartDiseaseProject/data/cardio_train.csv', sep=';')
+
